c9e2c992d36b76ef4cf2b52c73e930493ec44471
Yegorich75 <yegorich75@gmail.com>
2021-06-18 18:32:20 +0300

    HeaderInner bug

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
-  <div class="wrapper">
-    <Header />
-  </div>
+
+  <Header>
+    <HeaderInner />
+  </Header>
+
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import Header from './components/Header.vue'
+import HeaderInner from './components/HeaderInner.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
-    Header
+    Header,
+    HeaderInner
   }
 })
 </script>
